ee705ef844c819345364b9215c7c9f5cb4105859
Slynchy <sam.lynch2@hotmail.co.uk>
2015-03-24 01:58:58 +0000

    Third commit
    office added

--- script/feed.js
+++ script/feed.js
@@ -1,80 +1,129 @@
 var currentRoom = "1a";
 var currentRoomID = 0;
 var animatestatic = 0;
-var showrecord = false
+var showrecord = false;
+var feedopen = true;
+
 // roomstates: 0 = default, 1 = different, etc.
 var currRoomStates=[{name:"1a",roomstate:0},
-{name:"1b",roomstate:0}
+{name:"1b",roomstate:0},
+{name:"1c",roomstate:0}
 ];
 
-var rooms=[{name:"1a",movingcamera:true},
-{name:"1b",movingcamera:false}
+var rooms=[{name:"1a",movingcamera:true,leftadjustment:0},
+{name:"1b",movingcamera:false,leftadjustment:0},
+{name:"1c",movingcamera:false,leftadjustment:350}
 ];
 
-var animatronicStates = [{name:"Chica",currentRoom:"1a"},
-{name:"Bonny",currentRoom:"1a"}
-];
+function updateAIPosition(AIID,AIstate,newroom,roomstate,oldroomstate){
+    updateRoomState(animatronicStates[AIID].currentRoom,oldroomstate,1); //old room
+	updateAIState(AIID,AIstate,newroom);
+    console.log(animatronicStates[AIID].currentRoom);
+    updateRoomState(newroom,roomstate); //new room
+}
+
+var animatronicStates = [{name:"Chica",currentRoom:"1a",state:0},
+                         {name:"Bonnie",currentRoom:"1a",state:0},
+                         {name:"Freddy",currentRoom:"1a",state:0}
+						];
 
 function searchForState(room){
     for (var i=0; i < currRoomStates.length; i++) {
         if (currRoomStates[i].name === room) {
             return currRoomStates[i].roomstate;
-        }
-    }
-}
+        };
+    };
+};
 
 function searchForRoomID(roomname){
     for (var i=0; i < currRoomStates.length; i++) {
         if (rooms[i].name === roomname) {
             return i;
-        }
-    }
-}
+        };
+    };
+};
 
-var updateRoomState = function(roomname,state){
+var updateAIState = function(AIID,state,newroom,roomID){
+	if(state=="") state = 1;//parseInt(state);
+//    console.log(typeof roomID);
+//	if((typeof roomID) != "number") return console.log("Room name not an integer!");
+    if(newroom!=="") animatronicStates[AIID].currentRoom = newroom;
+    if(newroom!=="") animatronicStates[AIID].state = state;
+	
+	switch(AIID) {
+		case 0:// chica
+            switch(state) {
+                case 0: //unseen
+                    break;   
+                case 1: //seen
+                    break;
+            }
+            break;
+		case 1: //Bunny
+			break;
+		default:
+			alert("Invalid or no room name given!");
+	}
+	
+	return console.log("AI "+animatronicStates[AIID].name+" state updated");
+};
+
+var updateRoomState = function(roomname,state,timeout){
+    if(timeout=="") timeout=3000;
 	if(state=="") state = 1//parseInt(state);
 	if((typeof roomname) != "string") return console.log("Room name not a string!");
 	
 	switch(roomname) {
 		case "1a":
 			currRoomStates[0].roomstate=state;
-			if(currentRoom == roomname) updateRoomStateStatic() //document.getElementById('static').style.opacity="1";
+			if(currentRoom == roomname && state == currRoomStates[0].roomstate) updateRoomStateStatic(); //document.getElementById('static').style.opacity="1";
 			switch(state) {
 				case 0: // normal
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    };
 					break;
 				case 1: // just freddy
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    }
 					break;
 				case 2: // missing bunny
-					
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    };
 					break;
 				case 3: // missing chica
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    };
 					break;
 				case 4: // just freddy looking at camera
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    }
 					break;
 				case 5: // empty
-					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
-					}, 3000);
+                    if(currentRoom == roomname){
+                        setTimeout(function(){
+                            roomdiv.attr("src","graphics/rooms/1a/"+state+".png");
+                        }, timeout);
+                    }
 					break;
 				default:
 					alert("penis");
 			}
-			if(leftornot==0) document.getElementById('room').style.left="0";
+			if(leftornot==0) roomdiv.css("left","0");
 			break;
 		case "1b":
 			currRoomStates[1].roomstate=state;
@@ -82,13 +131,13 @@ var updateRoomState = function(roomname,state){
 			switch(state) {
 				case 0: // bonny
 					setTimeout(function(){
-						document.getElementById('room').src="graphics/rooms/1a/"+state+".png";
+						roomdiv.src="graphics/rooms/1a/"+state+".png";
 					}, 3000);
 					break;
 				default:
 					alert("penis");
 			}
-			if(leftornot==0 && currentRoom==roomname) document.getElementById('room').style.left="0";
+			if(leftornot==0 && currentRoom==roomname) roomdiv.css("left","0");
 			break;
 		default:
 			alert("Invalid or no room name given!");
@@ -104,25 +153,24 @@ var randomcheck=1
 
 function updatecurrentRoom(roomparameter) {
 	if(roomparameter=="") return console.log("updatecurrentRoom() error - No parameter given");
-	currentroomstatetoset = searchForState(roomparameter);
-	//alert(currentroomstatetoset);
-	if(currentroomstatetoset== -1) return console.log("updatecurrentRoom() error - Invalid parameter given");
 	currentRoom=roomparameter;
+	currentroomstatetoset = 0
+	currentroomstatetoset = searchForState(roomparameter);
+	currentroomstatetoset2 = searchForRoomID(roomparameter);
+	if(currentroomstatetoset== -1) return console.log("updatecurrentRoom() error - Invalid parameter given");
 	currentRoomID=searchForRoomID(roomparameter)
-	updateRoomStateStatic();
-	document.getElementById('room').src="graphics/rooms/"+currentRoom+"/"+currRoomStates[currentroomstatetoset].roomstate+".png";
-	if(currentRoom=="1b"){
-		$("#room").stop();
-		if(leftornot==1) {
-			$("#room").css( "left", "300" );
-			leftornot=1;
-		}
-		else {
-			//$("#room").css( "left", "0" );
-			$("#room").css( "left", "0" );
-			leftornot=0;
-		}
+	updateRoomStateStatic(175);
+	roomdiv.attr("src","graphics/rooms/"+currentRoom+"/"+currRoomStates[currentroomstatetoset2].roomstate+".png");
+	roomdiv.css("left","0");
+	if(rooms[currentRoomID].leftadjustment!==0){
+		roomdiv.css("left","-="+rooms[currentRoomID].leftadjustment);
+	};
+	if(rooms[currentRoomID].movingcamera==false){
+		roomdiv.removeClass("roomtest");
 	}
+	else {
+		roomdiv.addClass("roomtest");
+	};
 }
 
 for(x=1;x<9;x++){
@@ -139,27 +187,48 @@ function mainThread() {
 function recordTick() {
 //	if($("#record").css("display")=="block") {
 	if(showrecord==true) {
-		$("#record").css("left", '-=9999px');
+		recorddiv.css("left", '-=9999px');
 		showrecord=false;
 	}
 	else {
-		$("#record").css("left", '+=9999px');
+		recorddiv.css("left", '+=9999px');
 		showrecord=true;
 	};
 }
 
-function playanimation(path_to_file,speed,frames){
+function playanimation(path_to_file,speed,frames,elementID){
 	if(path_to_file=="") return console.log("No file specified!");
-	if(speed=="") speed = 50;
+//	if(speed=="") speed = 50;
 	if(frames=="") return console.log("No frame length specified!");
+	console.log(path_to_file);
+	console.log(speed);
+	console.log(frames);
 	for(x=1;x<(frames+1);x++){
-		setTimeout(fuckthatimageup[x], (speed*x));
+		frame=x;
+		setTimeout(setDivImg, (speed*x),path_to_file,x,elementID);
 	}
 }
 
-//mainThread();
+function setDivImg(){
+//	document.getElementById(elementID).src=pathtofile+"_"+frame2+".png"
+	setTimeout(function(){
+		fandiv.attr("src","graphics/rooms/office/fan_1.png");
+	},60);
+	setTimeout(function(){
+		fandiv.attr("src","graphics/rooms/office/fan_2.png");
+	},120);
+	setTimeout(function(){
+		fandiv.attr("src","graphics/rooms/office/fan_3.png");
+	},180);
+};
 
-camerapositionTick();
+//mainThread();
+document.getElementById("body").style.display="block";
+//camerapositionTick();
 var mainThreadID = setInterval('mainThread()', 1000);
-var cameraanimId = setInterval('camerapositionTick()', 8500);
+//var fananimThreadID = setInterval('playanimation("graphics/rooms/office/fan",900,3,"fan")', 905);
+//var cameraanimId = setInterval('camerapositionTick()', 8500);
 var recordanimId = setInterval('recordTick()', 1000);
+//playanimation("graphics/rooms/office/fan",900,3,"fan");
+setInterval("setDivImg()",180);
+
--- script/ui.js
+++ script/ui.js
@@ -1,35 +1,63 @@
+var roomdiv = $("#room");
+var staticdiv = $("#static");
+var staticimgdiv = $("#staticimg");
+var recorddiv = $("#record");
+var camerafeeddiv = $("#camerafeed");
+//var officediv = $("#office");
+var officemaindiv = $("#officemain");
+//var officeroomdiv = $("#officeroom");
+var fandiv = $("#fan");
+var officecamerarightdiv = $("#officecameraright");
+var officecameraleftdiv = $("#officecameraleft");
+var officeX=0
+var officeXInterval;
+var officeXOtherInterval;
 
+var button1adiv = $("#button-1a");
+var button1bdiv = $("#button-1b");
+var button1cdiv = $("#button-1c");
+var buttonopenclosecameradiv = $("#openclosecamera");
 
 function staticTick() {
   randomcheck++
   if(randomcheck>8) randomcheck=1;
-  var elem = document.getElementById('staticimg');
-  elem.src=staticanim[randomcheck].src;
+//  var elem = document.getElementById('staticimg');
+  staticimgdiv.src=staticanim[randomcheck].src;
 
   staticId = setTimeout('staticTick()', 30);
 }
 
-function updateRoomStateStatic(){
+function OpenCloseFeed() {
+	camerafeeddiv.toggle();
+//	officediv.toggle();
+	officemaindiv.toggle();
+//	officeroomdiv.toggle();
+	fandiv.toggle();
+}
+
+function updateRoomStateStatic(duration){
+    if(duration=="") duration = 2000;
 	if(animatestatic==0) {
 		animatestatic=1;
-		$("#static").animate({
+		staticdiv.animate({
 			opacity: 2
 		},0,function(){
 			animatestatic=2;
 			setTimeout(function(){
 				animatestatic=0;
-				$("#static").animate({
+				staticdiv.animate({
 					opacity: 0.25
 				},1000,function(){
 				});
-			}, 2000)
+			}, duration)
 		});
 	}
 }
 
 function camerapositionTick() {
+	//element.style.webkitTransform = "rotate(-2deg)";
 	if(rooms[currentRoomID].movingcamera!==false){
-		$("#room").animate({
+		roomdiv.animate({
 			left: leftpos,
 		},6000,function(){
 			if(leftornot==1) leftornot=0
@@ -41,41 +69,104 @@ function camerapositionTick() {
 }
 
 // ==============================================BUTTON EVENTS==============================================
-$('#button-1a').mouseenter(function(){
+button1adiv.mouseenter(function(){
 	if(this.id!==("button-"+currentRoom)){
 		this.style.backgroundColor='#505050';
 	}
 	else this.style.backgroundColor='#107010';
 });
-$('#button-1a').mouseleave(function(){
-	if(this.id!==("button-"+currentRoom)){
-		this.style.backgroundColor='#303030';
-	}
-});
-$('#button-1a').click(function(){
-	if(currentRoom!=="1a"){
-		this.style.backgroundColor='#107010';
-		$('#button-1b').css("backgroundColor",'#101010');
-		updatecurrentRoom("1a");
-	}
-});
-
-$('#button-1b').mouseenter(function(){
-	if(this.id!==("button-"+currentRoom)){
-		this.style.backgroundColor='#505050';
-	}
-	else this.style.backgroundColor='#107010';
-});
-$('#button-1b').mouseleave(function(){
+button1adiv.mouseleave(function(){
 	if(this.id!==("button-"+currentRoom)){
 		this.style.backgroundColor='#101010';
 	}
 });
-$('#button-1b').click(function(){
-	if(currentRoom!=="1b"){
+button1adiv.click(function(){
+	if(currentRoom!=="1a"){
 		this.style.backgroundColor='#107010';
-		$('#button-1a').css("backgroundColor",'#101010');
-		updatecurrentRoom("1b");
+		updatecurrentRoom("1a");
+		resetCameraButtons(currentRoom);
 	}
 });
+
+button1bdiv.mouseenter(function(){
+	if(this.id!==("button-"+currentRoom)){
+		this.style.backgroundColor='#505050';
+	}
+	else this.style.backgroundColor='#107010';
+});
+button1bdiv.mouseleave(function(){
+	if(this.id!==("button-"+currentRoom)){
+		this.style.backgroundColor='#101010';
+	}
+});
+button1bdiv.click(function(){
+	if(currentRoom!=="1b"){
+		this.style.backgroundColor='#107010';
+		updatecurrentRoom("1b");
+		resetCameraButtons(currentRoom);
+	}
+});
+
+button1cdiv.mouseenter(function(){
+	if(this.id!==("button-"+currentRoom)){
+		this.style.backgroundColor='#505050';
+	}
+	else this.style.backgroundColor='#107010';
+});
+button1cdiv.mouseleave(function(){
+	if(this.id!==("button-"+currentRoom)){
+		this.style.backgroundColor='#101010';
+	}
+});
+button1cdiv.click(function(){
+	if(currentRoom!=="1c"){
+		this.style.backgroundColor='#107010';
+		updatecurrentRoom("1c");
+		resetCameraButtons(currentRoom);
+	}
+});
+
+buttonopenclosecameradiv.click(function(){
+	OpenCloseFeed();
+});
+officecamerarightdiv.mouseenter(function(){
+	officeXInterval = setInterval(function(){
+		if(officeX>=420) return;
+		officeX=(officeX+20);
+		document.getElementById("office").style.webkitTransform="translate(-"+officeX+"px)";
+		console.log("Translated by +"+officeX);
+	},10);
+});
+officecamerarightdiv.mouseleave(function(){
+	clearInterval(officeXInterval);
+});
+officecameraleftdiv.mouseenter(function(){
+	officeXOtherInterval = setInterval(function(){
+		if(officeX==0) return;
+		officeX=(officeX-20);
+		document.getElementById("office").style.webkitTransform="translate(-"+officeX+"px)";
+		console.log("Translated by -"+officeX);
+	},10);
+});
+officecameraleftdiv.mouseleave(function(){
+	clearInterval(officeXOtherInterval);
+});
 // ==============================================END OF BUTTON EVENTS=======================================
+
+function resetCameraButtons(current){
+	for(x=0;x<rooms.length;x++){
+		if(rooms[x].name!==current){
+			eval("button"+rooms[x].name+"div.css('backgroundColor','#101010');");
+		}
+	}
+}
+/*
+function readMouseMove(e){
+	mousePosition = [];
+	mousePosition[0] = e.clientX;
+	mousePosition[1] = e.clientY;
+	return mousePosition;
+}
+document.onmousemove = readMouseMove;
+window.screen.availHeight
+*/
